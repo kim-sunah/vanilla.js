f4aa247d767ddae01763e800a2818016e72af298
sunah <1004_sunah@naver.com>
2023-09-17 07:19:43 +0900

    todoList

--- 5[2021 update]clock/js/quotes.js
+++ 5[2021 update]clock/js/quotes.js
@@ -2,32 +2,32 @@ const quotest = [
     {
         quote: "내일을 위해서 오늘을 살아라\n어제를 되돌아봤자 아무것도 찾을 수 없다\n내일을 어떻게 할지 그것을 생각하는게 인간이다",
         author: "블러드+",
-        image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcfDmYh%2FbtqEQZgwQZH%2FrndtliNclr1BiwShKIyJxK%2Fimg.jpg",
+        image: "https://image.fmkorea.com/files/attach/new2/20210304/2895716/3428664405/3430376180/4e3fd36b73ef0fe169fa13c5530cc40c.jpeg",
     },
     {
         quote: "포기하면 그 순간이 바로 시합 종료입니다",
         author: "슬램덩크",
-        image: "http://file3.instiz.net/data/file3/2018/03/14/e/e/3/ee3c06a0d3bd9617c2162d7570f3e132.jpg",
+        image: "https://www.bizhankook.com/upload/bk/article/202301/thumb/25093-60353-sample.jpg",
     },
     {
         quote: "인생이라...\n네 말대로 만화의 결말은 죄다 거기서 거기일지도 모르지.\n근데말이야, \n자기 인생에 관해선 너나 나나 독자는 아니야. 바로 작가지\n결말 정도 언제든지 다시 쓸 수 있잖아?",
         author: "은혼",
-        image: "http://file3.instiz.net/data/file3/2018/03/14/6/0/0/600171f0a67a14c4921bbac4699205d6.jpg",
+        image: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F1145D00D4B93A7D97C",
     },
     {
         quote: "너에게는 아직 꿈을 이루기 위한\n충분한 시간이 있어!",
         author: "피터팬",
-        image: "http://file3.instiz.net/data/file3/2018/03/02/c/0/a/c0a124286af26653f077517f08539a00.jpg",
+        image: "https://cdn.gjdream.com/news/photo/old/news/contents/UPFILE/2014/20140831107034.jpg",
     },
     {
         quote: "넘어지지 않는 사람은 없어.\n단, 다시 일어나는 사람만이\n앞으로 나아가는 법을 배우는 거야",
         author: "밤비",
-        image: "http://file3.instiz.net/data/file3/2018/03/14/3/9/d/39d72142eef965c7f7b7eeab0db87a85.jpg",
+        image: "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMjVfMjI5/MDAxNTc5OTI3NTEwMDY5.QRYbp_c-M5K_vkKUkREZtDPmKtBSO85ThTEX-lq0q7Yg.3YcZoPzH6xjd3-EkXok45DYcp0XijahlEeP18iFgMqgg.JPEG.c106507/SE-bf1f5fb5-f816-45aa-804c-74971f582eed.jpg?type=w800",
     },
     {
         quote: "과거는 흘러갔고 어쩔수 없는 거야, 그렇지?\n세상이 널 힘들게 할 땐\n신경끄고 사는게 상책이야",
         author: "라이온킹",
-        image: "http://file3.instiz.net/data/file3/2018/03/02/b/7/3/b73b5aa90f6ac506577cf8b9a16351cd.jpg",
+        image: "https://post-phinf.pstatic.net/MjAxOTA3MTZfMjc2/MDAxNTYzMjA1MDMxMTM3.KXdjxTrAXOfZDVkhGvjFgOYIQRQhYYJMxXZKxJ5JrIIg.DgswPSDlwgY8ZjX5dudFg4Qvi3i25oiKA-LqgR4N1pkg.JPEG/62.jpg?type=w800_q75",
     },
     {
         quote: "이걸 기억하겠다고 약속해줘.\n넌 네가 믿는 것보다 더 용감하며,\n보기보다 강하고\n네 생각보다 더 똑똑하다는 걸.",
@@ -52,12 +52,12 @@ const quotest = [
     {
         quote: "재능은 꽃비우는것\n센스는 갈고닦는것",
         author: "하이큐",
-        image: "https://mblogthumb-phinf.pstatic.net/MjAxOTA4MDZfMTIg/MDAxNTY1MDUyODk4Mjky.5OG6Y9zfjtRH0Rkic9mpIZTgJQxM0WDhpIf8lsVRc8Eg.GvS6FIsHYWVJvdB7knPIPsmskLSIK8uEvUHnW7j_Rhgg.PNG.nicole3183/Leopard-Raws_Haikyuu_-_20_RAW_(TBS_1280x720_x264_AAC).mp4_000188944.png?type=w800",
+        image: "https://i.namu.wiki/i/76SoMtuxTQsENOs0-z4LiZFM6Xp4tPOzydZfmCVyM89DZOzL6SFbnCBfL2NL7O22SAwg7eIAwV22avSc0n8M3w.webp",
     },
     {
         quote: "그래, 물론 인생은 고통과 고난으로 가득해.\n하지만 요령은 순간에 주어진\n몇몇 완벽한 경험들을 즐기는 거야!",
         author: "심슨가족",
-        image: "https://mblogthumb-phinf.pstatic.net/MjAyMDAzMDhfMjQx/MDAxNTgzNjY4NTQ4NjI3.ouKWTl9ceBX-HHnwYbwXtnQXKKEInIPucyPqMgBUkCEg.UmekOJ9rSt3-tQMrJHqJB38KKNp8CLNgmJfBmjQOm5cg.PNG.yunvestor1/심슨가족_명언_(0).PNG?type=w800",
+        image: "https://img1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/news/201611/16/ned/20161116102403306ttgg.jpg",
     },
     {
         quote: "해보면 되잖아?\n하고싶으니까 해본다\n정말로 하고싶은 것이란\n그런 느낌으로 시작하는거 아니야?",
@@ -67,7 +67,7 @@ const quotest = [
     {
         quote: "아직 멀었어\n'열심히 노력하고있다'란 말을 쉽게 내뱉을 수 있다는건\n아직 자신에게 관대하단 증거니까",
         author: "판도라 하츠",
-        image: "https://media.growthing.co.kr/media/quotes/quote_538_cGLuHGu.jpg",
+        image: "https://image.aniboxtv.com/aniboxtv_img/program/201106301516192090_s1.jpg",
     },
     {
         quote: "너의 미래가 장미빛일지\n암흑천지일지는\n모두 너에게 달린거야",
@@ -80,9 +80,9 @@ const quotest = [
         image: "https://i.ytimg.com/vi/EtuvhQZfmCI/maxresdefault.jpg",
     },
     {
-        quote: "자! 걱정할건 아무것도 없어!\n다들 앞만 봐\n든뒤는 내가 지켜줄게",
+        quote: "자! 걱정할건 아무것도 없어!\n다들 앞만 봐\n등 뒤는 내가 지켜줄게",
         author: "하이큐",
-        image: "https://t1.daumcdn.net/cfile/tistory/21652F5058213D4030",
+        image: "https://pbs.twimg.com/media/CqThSTeVUAEV-Se.jpg",
     },
     {
         quote: "난 평범하다고.\n그런데, 평범한 나야,\n바닥이나 보고 있을 시간은 있는거냐?",
@@ -103,8 +103,12 @@ author.innerText = choseQuote.author;
 
 /* background */
 
-const image = document.createElement("img")
+// const image = document.createElement("img")
 
-image.src=choseQuote.image;
+// image.src=choseQuote.image;
 
-document.body.prepend(image)
+// document.body.prepend(image)
+
+const imgTag = document.getElementById("image")
+imgTag.style.backgroundImage = "url("+choseQuote.image+")"
+console.log(imgTag)
---
+++ 5[2021 update]clock/js/todo.js
@@ -0,0 +1,58 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.getElementById("todo-list");
+
+const Todos_Key = "todos"
+
+let toDos = [];
+
+function saveTodo(){
+    localStorage.setItem(Todos_Key, JSON.stringify(toDos))
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id))
+    li.remove();
+    saveTodo();
+
+}
+
+function paintTodo(newTodo){
+    const li =  document.createElement("li")
+    li.id = newTodo.id
+    const span = document.createElement("span")
+    span.innerText = newTodo.text;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click",deleteTodo)
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li)
+}
+
+function handleTodoSubmit(event){
+    event.preventDefault();
+    const newTodo = todoInput.value;
+    todoInput.value="";
+    const newTodoObj = {
+        text: newTodo,
+        id:Date.now(),
+    }
+    toDos.push(newTodoObj)
+    paintTodo(newTodoObj)
+    saveTodo();
+}
+todoForm.addEventListener("submit", handleTodoSubmit);
+
+function sayHello(item){
+   paintTodo
+}
+
+const savedTodos=localStorage.getItem(Todos_Key)
+if(savedTodos != null){
+    const parsedTodos = JSON.parse(savedTodos);
+    toDos= parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
