800d9d64829709ff46cfcc8a2c58e7e3f97c6651
sunah <1004_sunah@naver.com>
2023-09-17 02:40:17 +0900

    명언 넣기

---
+++ 5[2021 update]clock/js/quotes.js
@@ -0,0 +1,103 @@
+const quotest = [
+    {
+        quote: "내일을 위해서 오늘을 살아라\n어제를 되돌아봤자 아무것도 찾을 수 없다\n내일을 어떻게 할지 그것을 생각하는게 인간이다",
+        author: "블러드+",
+        image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcfDmYh%2FbtqEQZgwQZH%2FrndtliNclr1BiwShKIyJxK%2Fimg.jpg",
+    },
+    {
+        quote: "포기하면 그 순간이 바로 시합 종료입니다",
+        author: "슬램덩크",
+        image: "http://file3.instiz.net/data/file3/2018/03/14/e/e/3/ee3c06a0d3bd9617c2162d7570f3e132.jpg",
+    },
+    {
+        quote: "인생이라...\n네 말대로 만화의 결말은 죄다 거기서 거기일지도 모르지.\n근데말이야, \n자기 인생에 관해선 너나 나나 독자는 아니야. 바로 작가지\n결말 정도 언제든지 다시 쓸 수 있잖아?",
+        author: "은혼",
+        image: "http://file3.instiz.net/data/file3/2018/03/14/6/0/0/600171f0a67a14c4921bbac4699205d6.jpg",
+    },
+    {
+        quote: "너에게는 아직 꿈을 이루기 위한\n충분한 시간이 있어!",
+        author: "피터팬",
+        image: "http://file3.instiz.net/data/file3/2018/03/02/c/0/a/c0a124286af26653f077517f08539a00.jpg",
+    },
+    {
+        quote: "넘어지지 않는 사람은 없어.\n단, 다시 일어나는 사람만이\n앞으로 나아가는 법을 배우는 거야",
+        author: "밤비",
+        image: "http://file3.instiz.net/data/file3/2018/03/14/3/9/d/39d72142eef965c7f7b7eeab0db87a85.jpg",
+    },
+    {
+        quote: "과거는 흘러갔고 어쩔수 없는 거야, 그렇지?\n세상이 널 힘들게 할 땐\n신경끄고 사는게 상책이야",
+        author: "라이온킹",
+        image: "http://file3.instiz.net/data/file3/2018/03/02/b/7/3/b73b5aa90f6ac506577cf8b9a16351cd.jpg",
+    },
+    {
+        quote: "이걸 기억하겠다고 약속해줘.\n넌 네가 믿는 것보다 더 용감하며,\n보기보다 강하고\n네 생각보다 더 똑똑하다는 걸.",
+        author: "곰돌이 푸",
+        image: "http://file3.instiz.net/data/file3/2018/03/02/f/8/8/f88c863b4c63b47252daf44045e35272.jpg",
+    },
+    {
+        quote: "아름답게 최후를 장식할 여유가 있다면\n끝까지 아름답게 살아야 하지 않겠나?",
+        author: "은혼",
+        image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F1145D00D4B93A7D97C",
+    },
+    {
+        quote: "걸려 넘어진걸\n돌부리 탓으로 돌리면\n뭐가 바뀌냐?",
+        author: "은혼",
+        image: "https://pbs.twimg.com/media/DiteKxsV4AAeQ8D.jpg",
+    },
+    {
+        quote: "의미없는것을 잔뜩하는것이\n인생이란다",
+        author: "마르코는 아홉살",
+        image: "https://i.ytimg.com/vi/GxDc890n27I/maxresdefault.jpg",
+    },
+    {
+        quote: "재능은 꽃비우는것\n센스는 갈고닦는것",
+        author: "하이큐",
+        image: "https://mblogthumb-phinf.pstatic.net/MjAxOTA4MDZfMTIg/MDAxNTY1MDUyODk4Mjky.5OG6Y9zfjtRH0Rkic9mpIZTgJQxM0WDhpIf8lsVRc8Eg.GvS6FIsHYWVJvdB7knPIPsmskLSIK8uEvUHnW7j_Rhgg.PNG.nicole3183/Leopard-Raws_Haikyuu_-_20_RAW_(TBS_1280x720_x264_AAC).mp4_000188944.png?type=w800",
+    },
+    {
+        quote: "그래, 물론 인생은 고통과 고난으로 가득해.\n하지만 요령은 순간에 주어진\n몇몇 완벽한 경험들을 즐기는 거야!",
+        author: "심슨가족",
+        image: "https://mblogthumb-phinf.pstatic.net/MjAyMDAzMDhfMjQx/MDAxNTgzNjY4NTQ4NjI3.ouKWTl9ceBX-HHnwYbwXtnQXKKEInIPucyPqMgBUkCEg.UmekOJ9rSt3-tQMrJHqJB38KKNp8CLNgmJfBmjQOm5cg.PNG.yunvestor1/심슨가족_명언_(0).PNG?type=w800",
+    },
+    {
+        quote: "해보면 되잖아?\n하고싶으니까 해본다\n정말로 하고싶은 것이란\n그런 느낌으로 시작하는거 아니야?",
+        author: "러브라이브",
+        image: "https://i3.ruliweb.com/img/5/6/7/0/5670D5B1394D14002A",
+    },
+    {
+        quote: "아직 멀었어\n'열심히 노력하고있다'란 말을 쉽게 내뱉을 수 있다는건\n아직 자신에게 관대하단 증거니까",
+        author: "판도라 하츠",
+        image: "https://media.growthing.co.kr/media/quotes/quote_538_cGLuHGu.jpg",
+    },
+    {
+        quote: "너의 미래가 장미빛일지\n암흑천지일지는\n모두 너에게 달린거야",
+        author: "도라에몽",
+        image: "https://miro.medium.com/v2/resize:fit:886/1*fVmoVn2raaQQFCU0KfFk6w.jpeg",
+    },
+    {
+        quote: "매일 행복하진 않지만\n행복한 '일'은 매일 있어",
+        author: "곰돌이푸",
+        image: "https://i.ytimg.com/vi/EtuvhQZfmCI/maxresdefault.jpg",
+    },
+    {
+        quote: "자! 걱정할건 아무것도 없어!\n다들 앞만 봐\n든뒤는 내가 지켜줄게",
+        author: "하이큐",
+        image: "https://t1.daumcdn.net/cfile/tistory/21652F5058213D4030",
+    },
+    {
+        quote: "난 평범하다고.\n그런데, 평범한 나야,\n바닥이나 보고 있을 시간은 있는거냐?",
+        author: "하이큐",
+        image: "https://i.namu.wiki/i/t50WIgggvL7TBZjHfz7pWtEav_LHNOw31WwctYMgCbzsvlRX1fX-HEENBfVSTUDU87P6gUsgKWaYbGQpmPtSTA.webp",
+    },
+]
+const randomNumber = Math.floor(Math.random() * quotest.length);
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const choseQuote=quotest[randomNumber]
+console.log(quotest[Math.floor(Math.random() * quotest.length)])
+console.log(quotest[randomNumber])
+
+console.log(choseQuote)
+quote.innerText = choseQuote.quote ; 
+author.innerText = choseQuote.author;
